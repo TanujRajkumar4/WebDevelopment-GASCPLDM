38ee9c6707cdf3e4cf441636bd1e349d8d6edc90
TANUJ RAJKUMAR <medicustanuj@gmail.com>
2021-10-30 12:03:16 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,192 +1,204 @@
 <!DOCTYPE html>
- <html dir="ltr" lang="en">
- <head>
- <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
- <meta name="viewport" content="width=device-width,initial-scale=1.0" />
- <meta charset="utf-8" />
- <meta name="description" content="Palladam || Government Arts and Science College" />
- <meta name="keywords" content="Government college palladam, Arts college palladam, Arts and science college palladam" />
- <meta name="author" content="shakthisoftsolutions" />
- <title>Puratchi Thalaivi Amma Government Arts and Science College, Palladam</title>
- <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
- <link rel="icon" type="image/png" href="img/favicon.png" />
- <link rel="stylesheet" href="css/bootstrap.min.css" />
- <link rel="stylesheet" href="css/font-awesome.min.css" />
- <link rel="stylesheet" href="css/magnific-popup.css" />
- <link rel="stylesheet" href="css/owl.carousel.min.css" />
- <link rel="stylesheet" href="css/animate.css" />
- <link rel="stylesheet" href="css/main.css" />
- <link rel="stylesheet" href="style.css" />
- <link rel="stylesheet" href="css/meanmenu.min.css" />
- <link rel="stylesheet" href="css/responsive.css" />
- </head>
- <body class="home-v2">
- <!-- <div id="preloader">
+<html dir="ltr" lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <meta name="description" content="Palladam || Government Arts and Science College" />
+  <meta name="keywords" content="Government college palladam, Arts college palladam, Arts and science college palladam" />
+  <meta name="author" content="shakthisoftsolutions" />
+  <title>Puratchi Thalaivi Amma Government Arts and Science College, Palladam</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+  <link rel="icon" type="image/png" href="img/favicon.png" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/font-awesome.min.css" />
+  <link rel="stylesheet" href="css/magnific-popup.css" />
+  <link rel="stylesheet" href="css/owl.carousel.min.css" />
+  <link rel="stylesheet" href="css/animate.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="css/meanmenu.min.css" />
+  <link rel="stylesheet" href="css/responsive.css" />
+</head>
+
+<body class="home-v2">
+  <!-- <div id="preloader">
  <div id="preloader-status"></div>
  </div> -->
 
 
- <header>
+  <header>
 
- <div class="header-top">
- <div class="container">
- <div class="row">
- <div class="col-md-7 col-sm-8">
- <div class="header-left">
- <ul>
- <li><i class="fa fa-phone"></i>+91 425 5251144 , +91 425 5251145</li>
- <li><i class="fa fa-envelope-o"></i><a href="" class="__cf_email__">principalpalladam@gmail.com</a></li>
- </ul>
- </div>
- </div>
- <div class="col-md-5 col-sm-4">
- <div class="header-right-div">
- <div class="soical-profile">
- <span class="social-title">Follow Us </span>
- <ul>
- <li><a href=""><i class="fa fa-facebook"></i></a></li>
- <li><a href=""><i class="fa fa-twitter"></i></a></li>
- <li><a href=""><i class="fa fa-google"></i></a></li>
- <li><a href=""><i class="fa fa-skype"></i></a></li>
- </ul>
- </div>
- </div>
- </div>
- </div>
- </div>
- </div>
+    <div class="header-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-sm-8">
+            <div class="header-left">
+              <ul>
+                <li><i class="fa fa-phone"></i>+91 425 5251144 , +91 425 5251145</li>
+                <li><i class="fa fa-envelope-o"></i><a href="" class="__cf_email__">principalpalladam@gmail.com</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-5 col-sm-4">
+            <div class="header-right-div">
+              <div class="soical-profile">
+                <span class="social-title">Follow Us </span>
+                <ul>
+                  <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                  <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                  <li><a href=""><i class="fa fa-google"></i></a></li>
+                  <li><a href=""><i class="fa fa-skype"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
- <div class="hd-top-sec">
- <div class="container">
- <div class="row">
+    <div class="hd-top-sec">
+      <div class="container">
+        <div class="row">
 
- <div class="col-md-2 col-sm-12 col-xs-8 menu-left">
- <div class="logo">
- <a href="index.html"><img src="img/govt-logo.jpg" style="height: 85px;" /></a>
- </div>
- </div>
- <div class="col-md-8 col-sm-12 col-xs-8 menu-center">
- <!-- <a href="index.html"><center style="padding-top: 20px;"><h1>Puratchi Thalaivi Amma Government Arts and Science College, Palladam</h1><h3>(Affiliated to Bharathiar University, Coimbatore) -->
-  <a href="index.html"><center style="padding-top: 20px;"><h1>புரட்சி தலைவி அம்மா அரசு கலை மற்றும் அறிவியல் கல்லூரி, பல்லடம்
-<br>ESTD - 2017
-</h3></center></a>
- </div>
- 
- <div class="col-md-2 col-sm-12 col-xs-8 menu-right">
- <div class="logo">
- <a href="index.html"><img src="img/govt-logo.jpg" style="height: 85px;" /></a>
- </div>
- </div>
- </div>
- </div>
- </div>
- <div class="hd-sec ">
-  <div class="container">
- <div class="row">
- <div class="mobile-nav-menu"></div>
- <div class="col-md-12 col-sm-9 menu-left">
- <div class="menu">
- <nav id="main-menu" class="main-menu">
- <ul>
-  <li class="active"><a href="index.php">Home </a>
- </li>
- <li><a href="#">About Us</a>
- <ul>
- <li><a href="./page.php?page_id=2">About The College</a></li>
- <li><a href="./page.php?page_id=3">Mission &amp; Vision</a></li>
- <li><a href="./page.php?page_id=4">Former Principal</a></li>
- <li><a href="./page.php?page_id=5">Alumni Association</a></li>
- </ul>
- </li>
- <li><a href="#">Administration </a>
- <ul>
- <li><a href="./page.php?page_id=7">Principal</a></li>
- <li><a href="./page.php?page_id=8">Academic Council</a></li>
- <li><a href="./page.php?page_id=9">College Committee <i class="fa fa-angle-right"></i></a>
- <ul>
- <li><a href="./page.php?page_id=10">UGC Committee</a></li>
- <li><a href="./page.php?page_id=11">NAAC</a></li>
- <li><a href="./page.php?page_id=12">Anti Ragging  </a></li>
- <li><a href="./page.php?page_id=13">Disciplinary Committee</a></li>
- <li><a href="./page.php?page_id=14">Placement</a></li>
- <li><a href="./page.php?page_id=15">Carrier Guidance</a></li>
- <li><a href="./page.php?page_id=16">Research Committee</a></li>
- </ul>
- </li>
-  <li><a href="./page.php?page_id=17">Finance Committee</a></li>
-  <li><a href="./page.php?page_id=18">College Office</a></li>
- <li><a href="./page.php?page_id=19">Annual Report</a></li>
- </ul>
- </li>
+          <div class="col-md-2 col-sm-12 col-xs-8 menu-left">
+            <div class="logo">
+              <a href="index.html"><img src="img/govt-clg.jpg" /></a>
+            </div>
+          </div>
+          <div class="col-md-8 col-sm-12 col-xs-8 menu-center">
+            <a href="index.html">
+              <center style="padding-top: 20px;">
+                <h1>Puratchi Thalaivi Amma Government Arts and Science College, Palladam</h1>
+                <h3>(Affiliated to Bharathiar University, Coimbatore)
+                  <a href="index.php">
+                    <center style="padding-top: 20px;">
+                      <h1>புரட்சித்தலைவி அம்மா அரசு கலை மற்றும் அறிவியல் கல்லூரி, பல்லடம்
+                        <br><br>ESTD - 2017
+                </h3>
+              </center>
+            </a>
+          </div>
 
- <li><a href="#">Academics </a>
- <ul>
- <li><a href="#">Programmes <i class="fa fa-angle-right"></i></a>
- <ul>
- <li><a href="#">UG Programmes </a></li>
- <li><a href="#">PG Programmes </a></li>
- <li><a href="#">Research Programmes </a></li>
- </ul>
- </li>
- <li><a href="#">Departments <i class="fa fa-angle-right"></i></a>
- <ul>
- <li><a href="department.php?dept_ID=1">Tamil Department </a></li>
- <li><a href="department.php?dept_ID=2">English Department </a></li>
- <li><a href="department.php?dept_ID=3">Mathematics Department </a></li>
- <li><a href="department.php?dept_ID=4">Commerce Department </a></li>
- <li><a href="department.php?dept_ID=5">Computer Science Department </a></li>
- <li><a href="department.php?dept_ID=6">Chemistry Department </a></li>
- <li><a href="department.php?dept_ID=7">Costume Design and Fashion Department </a></li>
- </ul>
- </li>
- <li><a href="./page.php?page_id=20">Scholarship </a></li>
- <li><a href="./page.php?page_id=21">Library</a></li>
- <li><a href="./page.php?page_id=22">Physical Education</a></li>
- <li><a href="./page.php?page_id=23">Academic Calendar</a></li>
- </ul>
- </li>
-<li><a href="#"><p1>Admission</p1></a>
-      <ul>
-	  <li><a href=" ">Government Reservation Policy</a></li>
-           <!-- <li><a href="admission-application.html">Application Form</a></li>
+          <div class="col-md-2 col-sm-12 col-xs-8 menu-right">
+            <div class="logo">
+              <a href="index.html"><img src="img/govt-logo.jpg" /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hd-sec ">
+      <div class="container">
+        <div class="row">
+          <div class="mobile-nav-menu"></div>
+          <div class="col-md-12 col-sm-9 menu-left">
+            <div class="menu">
+              <nav id="main-menu" class="main-menu">
+                <ul>
+                  <li class="active"><a href="index.php">Home </a>
+                  </li>
+                  <li><a href="#">About Us</a>
+                    <ul>
+                      <li><a href="./page.php?page_id=2">About The College</a></li>
+                      <li><a href="./page.php?page_id=3">Mission &amp; Vision</a></li>
+                      <!--li><a href="./page.php?page_id=4">Former Principal</a></li>
+ <li><a href="./page.php?page_id=5">Alumni Association</a></li-->
+                    </ul>
+                  </li>
+                  <li><a href="#">Administration </a>
+                    <ul>
+                      <li><a href="./page.php?page_id=7">Principal</a></li>
+                      <!-- <li><a href="./page.php?page_id=8">Academic Council</a></li> -->
+                      <li><a href="./page.php?page_id=9">College Committee <i class="fa fa-angle-right"></i></a>
+                        <ul>
+                          <!-- <li><a href="./page.php?page_id=10">UGC Committee</a></li> -->
+                          <!-- <li><a href="./page.php?page_id=11">NAAC</a></li> -->
+                          <li><a href="./page.php?page_id=12">Anti Ragging </a></li>
+                          <li><a href="./page.php?page_id=13">Disciplinary Committee</a></li>
+                          <li><a href="./page.php?page_id=14">Placement</a></li>
+                          <!-- <li><a href="./page.php?page_id=15">Carrier Guidance</a></li> -->
+                          <!--li><a href="./page.php?page_id=16">Research Committee</a></li-->
+                        </ul>
+                      </li>
+                      <!-- <li><a href="./page.php?page_id=17">Finance Committee</a></li> -->
+                      <li><a href="./page.php?page_id=18">Rules and Regulations</a></li>
+                      <!--li><a href="./page.php?page_id=19">Annual Report</a></li-->
+                    </ul>
+                  </li>
+
+                  <li><a href="#">Academics </a>
+                    <ul>
+                      <li><a href="#">Programmes <i class="fa fa-angle-right"></i></a>
+                        <ul>
+                          <li><a href="#">UG Programmes </a></li>
+                          <li><a href="#">PG Programmes </a></li>
+                          <!-- <li><a href="#">Research Programmes </a></li> -->
+                        </ul>
+                      </li>
+                      <li><a href="#">Departments <i class="fa fa-angle-right"></i></a>
+                        <ul>
+                          <li><a href="department.php?dept_ID=1">Department of Tamil </a></li>
+                          <li><a href="department.php?dept_ID=2">Department of English</a></li>
+                          <li><a href="department.php?dept_ID=3">Department of Mathematics </a></li>
+                          <li><a href="department.php?dept_ID=4">Department of Commerce <i class="fa fa-angle-right"></i> </a>
+                            <ul>
+                              <li><a href="department.php?dept_ID=8">Department of B.Com. (B&I) </a></li>
+                              <li><a href="department.php?dept_ID=9">Department of B.Com. (C.A.) </a></li>
+                            </ul>
+                          </li>
+                          <li><a href="department.php?dept_ID=5">Department of Computer Science</a></li>
+                          <li><a href="department.php?dept_ID=6">Department of Chemistry</a></li>
+                          <li><a href="department.php?dept_ID=7">Department of Costume Design and Fashion</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="./page.php?page_id=20">Scholarship </a></li>
+                      <li><a href="./page.php?page_id=21">Library</a></li>
+                      <!-- <li><a href="./page.php?page_id=22">Physical Education</a></li> -->
+                      <li><a href="img/calendar/Calender_2021_2022.pdf" target="_blank">Academic Calendar</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">
+                      <p1>Admission</p1>
+                    </a>
+                    <ul>
+                      <li><a href=" ">Government Reservation Policy</a></li>
+                      <!-- <li><a href="admission-application.html">Application Form</a></li>
             <li><a href="admission-prospectus.html">Prospectus</a></li>-->
-             <!--<li><a href="admission-rules.html">Rules of Reservation </a></li>-->
-            <li><a href=" ">Enclosure</a></li>
-            <!--<li><a href="admission-online-application.html">Online Application </a></li>-->
-            <li><a href=" ">Fee Structure</a></li>
-            <li><a href=" ">Courses Offered</a></li>
-            <li><a href=" ">Eligibility Criteria</a></li>
-            <li><a href="./contact.php">Contact</a></li>
-			<!--<li><a href="images/home/ug-IIIcounselling.pdf" target="_blank">UG-III Counselling</a></li>-->
-            </ul>
-          </li>
- <li><a href="./page.php?page_id=24">Facilities </a></li>
- <li><a href="#">Examination </a>
- <ul>
- <li><a href="./page.php?page_id=26">Syllabus </a></li>
- <li><a href="./page.php?page_id=27">Results</a></li>
- </ul>
- </li>
- <li><a href="#">Activities </a>
- <ul>
+                      <!--<li><a href="admission-rules.html">Rules of Reservation </a></li>-->
+                      <!--<li><a href="admission-online-application.html">Online Application </a></li>-->
+                      <li><a href=" ">Fee Structure</a></li>
+                      <li><a href=" ">Eligibility Criteria</a></li>
+                      <!--<li><a href="images/home/ug-IIIcounselling.pdf" target="_blank">UG-III Counselling</a></li>-->
+                    </ul>
+                  </li>
+                  <li><a href="./page.php?page_id=24">Facilities </a></li>
+                  <li><a href="#">Examination </a>
+                    <ul>
+                      <li><a href="./page.php?page_id=27">Results</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Activities </a>
+                    <!--ul>
  <li><a href="#">NCC </a></li>
  <li><a href="#">NSS</a></li>
  <li><a href="#">YRC</a></li>
  <li><a href="#">RRC</a></li>
  <li><a href="#">ECO Club</a></li>
- </ul>
- </li>
- <!-- <li><a href="">AICTE </a></li>
+ </ul-->
+                  </li>
+                  <!-- <li><a href="">AICTE </a></li>
  <li><a href="">IQAC </a></li> -->
- <li><a href="./contact.php">Contact </a></li>
- </ul>
- </nav>
- </div>
- </div>
+                  <li><a href="./contact.php">Contact </a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
 
- </div>
- </div>
- </div>
+        </div>
+      </div>
+    </div>
 
- </header>
+  </header>
