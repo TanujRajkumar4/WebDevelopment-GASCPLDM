a74c749555c431bac3eb1e31883a1c2c5a63d0b4
Indumathi24V <86820512+Indumathi24V@users.noreply.github.com>
2022-03-30 22:06:45 +0530

    img display fix

--- AdminPanel/events.php
+++ AdminPanel/events.php
@@ -169,13 +169,14 @@ echo "<script>window.location.href = 'index.php';</script>";
     $i=1;
 	foreach($results as $dept){
 ?>                            
+									 $pro_img=explode('../',$dept['events_id']);
 							<tr role="row" class="odd">
 								<td class="sorting_1"><?php echo $i;?></td>
 								<td><?php echo $dept['events_title'];?></td>
                                <td><?php $date_yy=date("d-m-Y",strtotime($dept['events_date'])); echo $date_yy;?></td>
 							   <td><?php echo $dept['events_desc'];?></td>
                                <td><img src="<?php echo $dept['events_photo'];?>" height="150px" width="150px"></td>
-                              <td><a href="eventsedit.php?id=<?php echo $dept['events_id'];?>">Edit</a>
+                              <td><a href="eventsedit.php?id=<?php echo $pro_img[1];?>">Edit</a>
 								<a href="eventsdelete.php?id=<?php echo $dept['events_id']; ?>" onclick="return myFunction()">Delete</a>
 								</td>
 							</tr>
